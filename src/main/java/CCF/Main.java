122c1c29b1eb1214f70ba8a9247f059cb8bfaa18
yujianmeng <yujian_fe@163.com>
2017-09-27 21:07:01 +0800

    LeetCode

--- src/main/java/CCF/Main.java
+++ src/main/java/CCF/Main.java
@@ -1,68 +1,88 @@
 package CCF;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
- * Created by yujian on 2017/9/26.
- * 问题描述
- 　　体育老师小明要将自己班上的学生按顺序排队。他首先让学生按学号从小到大的顺序排成一排，学号小的排在前面，然后进行多次调整。一次调整小明可能让一位同学出队，向前或者向后移动一段距离后再插入队列。
- 　　例如，下面给出了一组移动的例子，例子中学生的人数为8人。
- 　　0）初始队列中学生的学号依次为1, 2, 3, 4, 5, 6, 7, 8；
- 　　1）第一次调整，命令为“3号同学向后移动2”，表示3号同学出队，向后移动2名同学的距离，再插入到队列中，新队列中学生的学号依次为1, 2, 4, 5, 3, 6, 7, 8；
- 　　2）第二次调整，命令为“8号同学向前移动3”，表示8号同学出队，向前移动3名同学的距离，再插入到队列中，新队列中学生的学号依次为1, 2, 4, 5, 8, 3, 6, 7；
- 　　3）第三次调整，命令为“3号同学向前移动2”，表示3号同学出队，向前移动2名同学的距离，再插入到队列中，新队列中学生的学号依次为1, 2, 4, 3, 5, 8, 6, 7。
- 　　小明记录了所有调整的过程，请问，最终从前向后所有学生的学号依次是多少？
- 　　请特别注意，上述移动过程中所涉及的号码指的是学号，而不是在队伍中的位置。在向后移动时，移动的距离不超过对应同学后面的人数，如果向后移动的距离正好等于对应同学后面的人数则该同学会移动到队列的最后面。在向前移动时，移动的距离不超过对应同学前面的人数，如果向前移动的距离正好等于对应同学前面的人数则该同学会移动到队列的最前面。
- 输入格式
- 　　输入的第一行包含一个整数n，表示学生的数量，学生的学号由1到n编号。
- 　　第二行包含一个整数m，表示调整的次数。
- 　　接下来m行，每行两个整数p, q，如果q为正，表示学号为p的同学向后移动q，如果q为负，表示学号为p的同学向前移动-q。
- 输出格式
- 　　输出一行，包含n个整数，相邻两个整数之间由一个空格分隔，表示最终从前向后所有学生的学号。
- 样例输入
- 8
- 3
- 3 2
- 8 -3
- 3 -2
- 样例输出
- 1 2 4 3 5 8 6 7
+ * Created by yujian on 2017/9/27.
  */
 public class Main {
-
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int stuNum = scanner.nextInt();
-        int steps = scanner.nextInt();
-        int[] students = new int[stuNum];
-        for (int j=0;j<stuNum;j++)
-            students[j]=j+1;
-        for (int i=0;i<steps;i++){
-            int num = scanner.nextInt();
-            int space = scanner.nextInt();
-            int index = 0;
-            for (int j=0;j<stuNum;j++){
-                if (students[j]==num){
-                    index=j;
-                    break;
+        Scanner sc = new Scanner(System.in);
+        String line = "";
+        String html = "";
+        List<String> htmlContent = new LinkedList<String>();
+        boolean theFirst=true;
+        String texts = "";
+        while (sc.hasNextLine()){
+            line = sc.nextLine();
+            if (line.equals("eof"))
+                break;
+            if (!line.equals("")) {
+                char head = line.charAt(0);
+                switch (head) {
+                    case '#':
+                        theFirst = true;
+                        int sum = 1;
+                        while (line.charAt(sum) == '#')
+                            sum++;
+                        line = inLine(line);
+                        html = "<h" + sum + ">" + line.substring(sum).trim() + "</h" + sum + ">";
+                        break;
+                    case '*':
+                        line = inLine(line);
+                        if (theFirst) {
+                            htmlContent.add("<ul>");
+                            htmlContent.add("</ul>");
+                            theFirst = false;
+                        }
+                        html = "<li>"+line.substring(1).trim()+"</li>";
+                        break;
+                    default:
+                        theFirst = true;
+                        texts = texts+line;
+                }
+                if (theFirst) htmlContent.add(html);
+                else htmlContent.add(htmlContent.size()-1,html);
+            }
+            else if (!texts.equals("")){
+                line = inLine(texts);
+                html = "<p>"+line.trim()+"</p>";
+                texts = "";
+                htmlContent.add(html);
+            }
+        }
+        for (int i=0;i<htmlContent.size();i++)
+            System.out.println(htmlContent.get(i));
+    }
+    public static String inLine(String content){
+        int flag = 1;
+        if(content.contains("_")){
+            for (int i=0;i<content.length();i++)
+                if (content.charAt(i)=='_'){
+                    if(flag++%2==1)
+                        content = content.replaceFirst("_","<em>");
+                    else content = content.replaceFirst("_","</em>");
+                }
+        }
+        if (content.contains("[")&&content.contains("(")){
+            int m=-1,n=-1;
+            int p=-1,q=-1;
+            for (int i=0;i<content.length();i++){
+                char c = content.charAt(i);
+                switch (c){
+                    case '[':m=i;break;
+                    case ']':n=i;break;
+                    case '(':p=i;break;
+                    case ')':q=i;break;
+                }
+                if (m>=0&&n>=0&&p>=0&&q>=0) {
+                    content = content.replace(content.substring(m, q + 1),
+                            "<a href=" + content.substring(p+1, q) + ">" + content.substring(m+1, n) + "</a>");
+                    i=0;
+                    m=-1;n=-1;p=-1;q=-1;
                 }
             }
-            if (space>0){
-                for (int m=index;m<index+space;m++)
-                    students[m]=students[m+1];
-                students[index+space]=num;
-            }
-            else if (space<0){
-                for (int m=index;m>index+space;m--)
-                    students[m]=students[m-1];
-                students[index+space]=num;
-            }
         }
-
-        for (int i=0;i<stuNum;i++) {
-            if (i == stuNum - 1)
-                System.out.println(students[i]);
-            else System.out.print(students[i] + " ");
-        }
+        return content;
     }
 }
