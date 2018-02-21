0a430cf9613db3c09b2216d442497acf4dd9836d
yujianmeng <yujian_fe@163.com>
2018-02-21 10:27:37 +0800

    ListIntersect

---
+++ src/main/java/facetime/ListIntersect.java
@@ -0,0 +1,62 @@
+package facetime;
+
+/**
+ * Created by yujian on 2018/2/21.
+ */
+public class ListIntersect {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null||headB==null){
+            return null;
+        }
+        ListNode p=headA;
+        ListNode q=headB;
+        int length1=0;
+        int length2=0;
+        int diff=0;
+        while(p!=null){
+            length1++;
+            p=p.next;
+        }
+        while(q!=null){
+            length2++;
+            q=q.next;
+        }
+        if(length1>length2){
+            p=headA;
+            q=headB;
+            diff=length1-length2;
+        }
+        else{
+            p=headB;
+            q=headA;
+            diff=length2-length1;
+        }
+        for(int i=0;i<diff;i++){
+            p=p.next;
+        }
+        while(p!=null&&q!=null){
+            if(p.equals(q)){
+                return p;
+            }
+            p=p.next;
+            q=q.next;
+        }
+        return null;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
+
+
